31bf1b60167a220b8afefbbe958ec4002b1a6ac7
Kóbori Bence <kobori.bence@stud.uni-obuda.hu>
2025-12-09 19:48:25 +0100

    pmanager comp created

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonManagerComponent } from './person-manager/person-manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonManagerComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/person-manager/person-manager.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-manager',
+  standalone: false,
+  templateUrl: './person-manager.component.html',
+  styleUrl: './person-manager.component.scss'
+})
+export class PersonManagerComponent {
+
+}
